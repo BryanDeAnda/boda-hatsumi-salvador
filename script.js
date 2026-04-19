70a9e4220caa44672188bb67ce9be6e59cc77dab
BryanDeAnda <bryan.deanda1955@alumnos.udg.mx>
2026-04-19 14:15:57 -0600

    animacion al hacer scroll

--- script.js
+++ script.js
@@ -34,3 +34,20 @@ document.addEventListener("DOMContentLoaded", () => {
     segundosEl.innerText = segundos;
   }, 1000);
 });
+
+const elements = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+elements.forEach((el) => observer.observe(el));
